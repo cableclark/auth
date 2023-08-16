56e337972899961c1c8536d8d03e0850ab3407fb
Igor Stojanovski <cableclark@gmail.com>
2023-08-16 15:39:27 +0200

    added a router

--- public/index.php
+++ public/index.php
@@ -1,5 +1,72 @@
 <?php 
 
+
+
 require __DIR__ . '/../src/bootstrap.php';
 
+
+class Router {
+
+    private $routes = [];
+
+
+    public function handle ($uri) {
+       
+
+        
+
+        foreach ($this->routes as $route) {
+            
+        
+            if ($route["route"] === $uri) {
+
+             require __DIR__ . '/../src/'. $route["route"] . '.php';
+            }
+
+        }
+
+
+       
+
+    }
+
+    public function add ($route) {
+       
+        $this->routes[$route] = ["route" => $route];
+        
+        
+    }
+
+    public function post ($route) {
+
+        $this->add($route);
+       
+        array_push($this->routes[$route], ["method"=>'post']);
+
+
+        return $this;
+        
+    }
+
+    public function get ($route) {
+
+        $this->add($route);
+       
+        array_push($this->routes[$route], ["method"=>'get']);
+
+        return $this;
+        
+    }
+
+}
+$router= new Router();
+
+$router->post("/register");
+
+$router->post("/login");
+
+$router->handle($_SERVER['REQUEST_URI']);
+
 require __DIR__ . '/../views/index.php';
+
+
